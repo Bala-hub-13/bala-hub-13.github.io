146880c4c99c299828ada2305d74264c97921530
Bala Murali <bala2004murali@gmail.com>
2025-08-15 20:23:50 +0530

    site update

--- cube.js
+++ cube.js
@@ -1,13 +1,6 @@
-      const cubeImg = document.querySelector('a[href="/"] img');
-      cubeImg.addEventListener('mouseover', function() {
-        this.style.transform = 'rotate(10deg)';
-        this.style.opacity = '1';
-      });
-      cubeImg.addEventListener('mouseout', function() {
-        this.style.transform = 'rotate(0deg)';
-        this.style.opacity = '0.8';
-      });
-        document.addEventListener("DOMContentLoaded", function() {
-    const cube = document.getElementById('home-cube');
+document.addEventListener("DOMContentLoaded", function () {
+  const cube = document.getElementById('home-cube');
+  if (cube) {
     requestAnimationFrame(() => cube.classList.add('loaded'));
-  });
+  }
+});
