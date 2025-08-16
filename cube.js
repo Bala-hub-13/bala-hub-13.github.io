6f132558a394f6f465db1d5b3696adce128ab4cb
Bala Murali <bala2004murali@gmail.com>
2025-08-16 09:34:58 +0530

    site update

--- cube.js
+++ cube.js
@@ -1,14 +1,15 @@
 const cube = document.getElementById('home-cube');
 
 window.addEventListener('scroll', () => {
-  if (window.scrollY > 0) {
-    // user has scrolled down → hide cube
-    cube.classList.add('hidden');
-  } else {
-    // back to very top → show cube
+  if (window.scrollY === 0) {
+    // At top → show cube
     cube.classList.remove('hidden');
+  } else {
+    // Anywhere else → hide cube
+    cube.classList.add('hidden');
   }
 });
+
 
 document.addEventListener("DOMContentLoaded", function () {
   const cube = document.getElementById('home-cube');
